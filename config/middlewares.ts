a929d5a8d7a06bc3538f354f6370637e0bb53e1f
Kurt Lewin <lewin_jota96@yahoo.com>
2026-04-08 20:01:35 +0800

    fix cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,16 +1,53 @@
-import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
 const config: Core.Config.Middlewares = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "res.cloudinary.com",
+            "tefs-photobooth-cms.onrender.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "res.cloudinary.com",
+            "tefs-photobooth-cms.onrender.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: "strapi::cors",
+    config: {
+      origin: [
+        "http://localhost:3000",
+        "http://localhost:1337",
+        "https://tefs-photobooth-cms.onrender.com",
+      ],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      keepHeaderOnError: true,
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
 
 export default config;

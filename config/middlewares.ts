92290b884932ac98e97b6262efd9030913ea2df1
Kurt Lewin <lewin_jota96@yahoo.com>
2026-04-08 21:07:55 +0800

    revert mid

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,45 +3,8 @@ import type { Core } from "@strapi/strapi";
 const config: Core.Config.Middlewares = [
   "strapi::logger",
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "res.cloudinary.com",
-            "tefs-photobooth-cms.onrender.com",
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "res.cloudinary.com",
-            "tefs-photobooth-cms.onrender.com",
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  {
-    name: "strapi::cors",
-    config: {
-      origin: [
-        "https://tefs-photobooth-cms.onrender.com",
-        "http://localhost:10000",
-        "http://localhost:1337",
-      ],
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      keepHeaderOnError: true,
-    },
-  },
+  "strapi::security",
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
